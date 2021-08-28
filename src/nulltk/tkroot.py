5cc16a5b99f16624539cc55b5c4f6e4b31ff9eac
Nicholas Hammond <nhammond129@gmail.com>
2021-08-28 00:02:44 -0500

    Fix missing comma

--- src/nulltk/tkroot.py
+++ src/nulltk/tkroot.py
@@ -2,6 +2,6 @@ import tkinter as tk
 from .style import Style
 
 class Tk(tk.Tk):
-    def __init__(self, *args, style=Style() **kwargs):
+    def __init__(self, *args, style=Style(), **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         style.apply(self)
